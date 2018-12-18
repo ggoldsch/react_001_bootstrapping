516565bd9667d4ebc12c554c9e5f3866b985b809
george goldschmied <george.goldschmied@verizon.net>
2018-12-18 13:42:58 -0500

    after convert to APP

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+export default class App extends React.Component{
+	render(){
+	 return <div className="jumbotron jumbotron-fluid text-sm-center"
+	         style={{backgroundImage:'url(img/fall-1072821_1920.jpg)', color:'white'}}>
+	            <div className="container">
+	              <h1>Building React.js User Interfaces</h1>
+	              <p>with Bootstrap and SASS.</p>
+	              <p><a className="btn btn-primary" href="http://02geek.com/" target="_blank">Discover things.</a></p>
+	            </div>
+	        </div>;
+	}	
+}	
--- src/client.js
+++ src/client.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './app'
 
-const component = 
-	<div className="jumbotron jumbotron-fluid text-sm-center" 
-		style={{backgroundImage:'url(img/fall-1072821_1920.jpg)', color:'white'}}>
-	          <div className="container">
-	          <h1>Building React.js User Interfaces</h1>
-	          <p>with Bootstrap and SASS.</p>
-	          <p><a className="btn btn-primary" href="http://02geek.com/" target="_blank">Discover things.</a></p>
-	          </div>
-	</div>;
-
-ReactDOM.render(component,document.getElementById('react'));
+ReactDOM.render(<App />,document.getElementById('react'));
 
