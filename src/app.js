344430a18fb2ec350b888608629d3f611bbbc58b
george goldschmied <george.goldschmied@verizon.net>
2018-12-19 15:40:42 -0500

    12/19/18

--- src/app.js
+++ src/app.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import Button from './bootstrap/button';
+import Jumbotron from './bootstrap/jumbotron';
 
 export default class App extends React.Component{
 	render(){
-	 return <div className="jumbotron jumbotron-fluid text-sm-center"
+	 return <Jumbotron className="-fluid text-sm-center"
 	         style={{backgroundImage:'url(img/fall-1072821_1920.jpg)', color:'white'}}>
 	            <div className="container">
 	              <h1>Building React.js User Interfaces</h1>
 	              <p>with Bootstrap and SASS.</p>
 	              <p>
-			<Button className="btn-primary" 
+			<Button className="-primary" 
 			  href="http://02geek.com/" target="_blank" disabled />
-			<Button className="-secondary" disabled />
-			<Button className="-danger-outline -sm" 
-			  href="http://02geek.com/" disabled />
+			<Button className="-danger-outline -sm" disabled />
 			</p>
 			<Button className="-primary-outline -lg -block" disabled />
 	            </div>
-	        </div>;
+	        </Jumbotron>;
 	}	
 }	
