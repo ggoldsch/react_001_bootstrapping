07e43df705fd1069e9fa26311d37a7c6302c377e
george goldschmied <george.goldschmied@verizon.net>
2018-12-18 20:02:56 -0500

    12/18

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Button from './bootstrap/button';
+
 export default class App extends React.Component{
 	render(){
 	 return <div className="jumbotron jumbotron-fluid text-sm-center"
@@ -6,7 +8,14 @@ export default class App extends React.Component{
 	            <div className="container">
 	              <h1>Building React.js User Interfaces</h1>
 	              <p>with Bootstrap and SASS.</p>
-	              <p><a className="btn btn-primary" href="http://02geek.com/" target="_blank">Discover things.</a></p>
+	              <p>
+			<Button className="btn-primary" 
+			  href="http://02geek.com/" target="_blank" />
+			<Button className="btn-secondary" />
+			<Button className="btn-danger-outline btn-sm" 
+			  href="http://02geek.com/" />
+			</p>
+			<Button className="btn-primary-outline btn-lg btn-block" />
 	            </div>
 	        </div>;
 	}	
