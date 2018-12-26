1ac70868a4582f452a6ec8af9a20935d854db762
george goldschmied <george.goldschmied@verizon.net>
2018-12-26 11:10:11 -0500

    still working

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import './appsource.scss';
 
 export default class App extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.onClick = this.onClick.bind(this);
+        this.state = {click:0, labels:[]};
+    }
+    onClick(e){
+        let labels = this.state.labels.concat([this.state.click]);
+        this.setState({click:this.state.click+1, labels:labels});
+        console.log("hello btn", this.state.click);
+        //console.log(this);
+    }
+
 	render(){
+        let buttons = this.state.labels.map((value, i)=>{    // returns an array of values
+            return <Button key={value} className="-secondary" >{value}</Button>;
+        });
         const items = <Jumbotron className="-fluid custom" containerFluid={true}>
 	              <h1>Building React.js User Interfaces</h1>
 	              <p>with Bootstrap and SASS.</p>
@@ -14,11 +30,16 @@ export default class App extends React.Component{
 			  href="http://02geek.com/" target="_blank" >Other way</Button>
 			<Button className="-danger -sm" label="Discover stuff" />
 			</p>
-			<Button className="-primary -lg -block" label="Discover things and stuff"/>
+
+			<Button onClick={this.onClick} className="-primary -lg -block">{this.state.click}</Button>
+
+            {buttons}
          </Jumbotron>;
 
 	 return (<ReactCSSTransitionGroup 
                 transitionName="example" 
+                transitionAppear={true}
+                transitionAppearTimeout={500} 
                 transitionEnterTimeout={500} 
                 transitionLeaveTimeout={300}>
         {items}
